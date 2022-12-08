35cc106c67378f4b5d4a426e517ce7ff886964b4
justin <pulchritude007@163.com>
2022-12-08 11:03:13 +0800

    submit spilt-database

---
+++ split-database/src/main/resources/sql/clean.sql
@@ -0,0 +1,6 @@
+-- 清理数据
+use ds_0;
+delete from t_user;
+
+use ds_1;
+delete from t_user;
---
+++ split-database/src/main/resources/sql/user.sql
@@ -0,0 +1,26 @@
+create database ds_0;
+create database ds_1;
+
+use ds_0;
+CREATE TABLE `t_user` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `name` varchar(64) DEFAULT NULL COMMENT '姓名',
+  `sex` varchar(32) DEFAULT NULL COMMENT '性别',
+  `age` int DEFAULT NULL COMMENT '年龄',
+  `create_time` datetime(6) DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime(6) DEFAULT NULL COMMENT '更新时间',
+  `status` tinyint(1) DEFAULT NULL COMMENT '是否删除 1删除 0未删除',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
+
+use ds_1;
+CREATE TABLE `t_user` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `name` varchar(64) DEFAULT NULL COMMENT '姓名',
+  `sex` varchar(32) DEFAULT NULL COMMENT '性别',
+  `age` int DEFAULT NULL COMMENT '年龄',
+  `create_time` datetime(6) DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime(6) DEFAULT NULL COMMENT '更新时间',
+  `status` tinyint(1) DEFAULT NULL COMMENT '是否删除 1删除 0未删除',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
